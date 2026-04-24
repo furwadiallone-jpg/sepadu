bc2b104bcd300c50132651dd13e421694ff71bdf
furwadiallone-jpg <furwadiallone@gmail.com>
2026-04-24 15:58:22 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/",(req,res) =>{
-  res.sendFile(path.resolve(__dirname, '../public/index.html'))
+  res.sendFile(path.resolve(__dirname, '../public/website/index.html'))
 });
 
 // Pastikan ini diletakkan di bawah semua route lainnya
